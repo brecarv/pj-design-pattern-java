4a3398aed14e28c24126a7ba6e5aec48808f79cf
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-05-30 14:17:01 -0300

    SingletonLazy

---
+++ src/design/pattern/gof/SingletonLazy.java
@@ -0,0 +1,15 @@
+package design.pattern.gof;
+
+public class SingletonLazy {
+    private static SingletonLazy instancia;
+
+    private SingletonLazy() {
+    }
+
+    public static SingletonLazy getInstancia() {
+        if (instancia == null) {
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+    }
+}
